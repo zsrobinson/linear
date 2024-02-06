cbe4af588233aa2f87a1abb8505573115557a541
Zachary Robinson <me@zsrobinson.com>
2024-02-06 17:08:44 -0500

    correct for floating point math

--- app/page.tsx
+++ app/page.tsx
@@ -129,11 +129,12 @@ function reducedEcholonForm(matrix: number[][]) {
   for (let currRow = 0; currRow < matrix.length; currRow++) {
     // STEP 1: start with first non-zero column as pivot
     const pivotPos = getFirstNonZeroCol(matrix, currRow);
+    if (pivotPos === -1) continue;
 
     // STEP 2: swap rows to bring non-zero term to pivot
-    if (matrix[currRow][pivotPos] === 0) {
+    if (isZero(matrix[currRow][pivotPos])) {
       for (let i = currRow + 1; i < matrix.length; i++) {
-        if (matrix[i][pivotPos] === 0) continue;
+        if (isZero(matrix[i][pivotPos])) continue;
         opr.interchange(currRow, i);
         break;
       }
@@ -141,7 +142,7 @@ function reducedEcholonForm(matrix: number[][]) {
 
     // STEP 3: perform row operations to clear below pivot
     for (let i = currRow + 1; i < matrix.length; i++) {
-      if (matrix[i][pivotPos] === 0) continue;
+      if (isZero(matrix[i][pivotPos])) continue;
 
       const scalar = -1 * (matrix[i][pivotPos] / matrix[currRow][pivotPos]);
       opr.replace(i, currRow, scalar);
@@ -151,6 +152,7 @@ function reducedEcholonForm(matrix: number[][]) {
   // STEP 5: working backwards, generate zeros above each pivot
   for (let currRow = matrix.length - 1; currRow >= 0; currRow--) {
     const pivotPos = getFirstNonZeroCol(matrix, currRow);
+    if (pivotPos === -1) continue;
 
     // ignore if pivot is 0, or scale current row so that pivot is 1
     if (matrix[currRow][pivotPos] === 0) {
@@ -170,14 +172,20 @@ function reducedEcholonForm(matrix: number[][]) {
   return opr.operations;
 }
 
-function getFirstNonZeroCol(matrix: number[][], startingRow = 0) {
-  for (let i = startingRow; i < matrix[startingRow].length; i++) {
+function getFirstNonZeroCol(matrix: number[][], startingRow: number) {
+  for (let i = 0; i < matrix[0].length; i++) {
+    let allZeros = true;
     for (let j = startingRow; j < matrix.length; j++) {
-      if (matrix[j][i] !== startingRow) continue;
+      if (!isZero(matrix[j][i])) allZeros = false;
     }
-    return i;
+    if (!allZeros) return i;
   }
   return -1;
+}
+
+/** Floating point math is screwing everything up so this is a band-aid */
+function isZero(n: number) {
+  return Math.abs(n) < 1e-5;
 }
 
 function strToMatrix(str: string) {
